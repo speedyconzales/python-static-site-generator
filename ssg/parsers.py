64bd0a01061a77083892239985def4ebee15b60f
speedyconzales <christoph.kloeffel@accenture.com>
2021-10-21 15:44:51 +0200

    second module done

--- ssg/parsers.py
+++ ssg/parsers.py
@@ -4,33 +4,28 @@ import shutil
 
 class Parser():
 
-    def __init__(self):
-        self.extensions: List[str] = []
+    extensions: List[str] = []
 
     def valid_extension(self,extension):
-        if extension in self.extensions:
-            return True
-        else:
-            return False
+        return extension in self.extensions
 
-    def parse(self,path,source,dest):
-        self.path = Path(path)
-        self.source = Path(source)
-        self.dest = Path(dest)
+    def parse(self,path: Path,source: Path,dest:Path):
         raise NotImplementedError
 
     def read(self,path):
-        with open(path) as file:
+        with open(path,"r") as file:
             return file.read()
 
     def write(self,path,dest,content,ext=".html"):
-        self.full_path = self.dest / path.with_suffix(ext).name
+        full_path = dest / path.with_suffix(ext).name
+        with open(full_path,"w") as file:
+            file.write(content)
 
     def copy(self,path,source,dest):
         shutil.copy2(path,dest / path.relative_to(source))
 
 class ResourceParser(Parser):
-    self.extensions = [".jpg",".png",".gif",".css",".html"]
+    extensions = [".jpg",".png",".gif",".css",".html"]
 
     def __init__(self):
         super().__init__()
