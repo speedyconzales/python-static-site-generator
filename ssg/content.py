4b2323950e21f3213df17aea1616aadaa99b6d68
speedyconzales <christoph.kloeffel@accenture.com>
2021-10-23 16:47:53 +0200

    done module3

--- ssg/content.py
+++ ssg/content.py
@@ -1,47 +1,38 @@
 import re
-from yaml import FullLoader,yaml
+from yaml import FullLoader,load
 from collections.abc import Mapping
 
 class Content(Mapping):
-    __delimiter = "^(?:-|\+){3}\s*$"
+    __delimiter = r"^(?:-|\+){3}\s*$"
     __regex = re.compile(__delimiter,re.MULTILINE)
 
-    def load(self,cls,string):
-        _,fm,content = __regex.split(string,2)
-        load(fm,FullLoader)
+    @classmethod
+    def load(cls,string):
+        _,fm,content = cls.__regex.split(string,2)
+        metadata = load(fm,Loader=FullLoader)
         return cls(metadata,content)
 
     def __init__(self,metadata,content):
         self.data = metadata
-        self.data.add({"content": content})
+        self.data["content"] = content
 
-    class @def body():
-        doc = "The body property."
-        def fget(self):
-            return self._body
-        def fset(self, value):
-            self._body = value
-        def fdel(self):
-            del self._body
+    @property
+    def body(self):
         return self.data["content"]
-    body = property(**body())
 
-    class @def type():
-        doc = "The type property."
-        def fget(self):
-            return self._type
-        def fset(self, value):
-            self._type = value
-        def fdel(self):
-            del self._type
-        return self.data["type"] if self.data["type"] else None
-    type = property(**type())
+    @property
+    def type(self):
+        return self.data["type"] if "type" in self.data else None
+
+    @type.setter
+    def type(self,type):
+        self.data["type"] = type
 
     def __getitem__(self,key):
         return self.data[key]
 
     def __iter__(self):
-        self.data.iter()
+        self.data.__iter__()
 
     def __len__(self):
         return len(self.data)
